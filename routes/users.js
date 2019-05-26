af3c9b4e9b0cb50dd52ae4774d895f8ee681e707
akjeres <akjeres@gmail.com>
2019-05-26 20:22:14 +0300

    Returning user with set _id]

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,40 @@ var fs = require('fs-extra');
 const { saveName, getUserList } = require("../services/user.service");
 const { isAuthorized } = require("../middlewares/auth.middleware");
 
-router.get('/', function(req, res, next) {
+router.get('/:id', isAuthorized, function(req, res, next) {
+  fs.readJson('./files/userlist.json')
+  .then(resolve => {
+    userList = resolve;
+    let status = 404,
+    userData = `User with _id = ${req.params.id} is not defined`;
+    for (let i = 0; i < resolve.length; i++) {
+      let currentUser = resolve[i];
+
+      if (req.params.id == currentUser['_id']) {
+        status = 200;
+        userData = currentUser;
+        break;
+      }
+    }
+    res.status(status).send(userData);
+  },
+    reject => {
+      status = 500;
+      throw new Error('User list is invalid');
+    }
+  )
+  .catch(err => {
+    res.status(status).send(err.message)
+  })
+});
+
+router.get('/', isAuthorized, function(req, res, next) {
   fs.readJson('./files/userlist.json')
   .then(packageObj => {
     res.send(packageObj)
   })
   .catch(err => {
-    res.status(500).send(`User List is not defined<br>${err}`)
+    res.status(404).send(`User List is invalid<br>${err}`)
   })
 });
 
